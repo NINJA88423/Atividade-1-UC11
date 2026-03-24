72354b912b7dc25dcfe9e3b47b5ff52a28efd96f
NINJA88423 <cesar.smiguel89@gmail.com>
2026-03-23 22:10:50 -0300

    Criação do sistema de cadastro
    Agora é possível cadastrar produtos no sistema

---
+++ LeiloesTDSat/src/ProdutosDAO.java
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Adm
+ */
+
+import java.sql.PreparedStatement;
+import java.sql.Connection;
+import javax.swing.JOptionPane;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+
+public class ProdutosDAO {
+    
+    Connection conn;
+    PreparedStatement prep;
+    ResultSet resultset;
+    ArrayList<ProdutosDTO> listagem = new ArrayList<>();
+    
+    public void cadastrarProduto (ProdutosDTO produto){
+        
+        try {
+            conn = new conectaDAO().connectDB();
+            prep = conn.prepareStatement("INSERT INTO `produtos` (`nome`, `valor`, `status`) VALUES (?, ?, ?)");
+            prep.setString(1, produto.getNome());
+            prep.setInt(2, produto.getValor());
+            prep.setString(3, produto.getStatus());
+            prep.executeUpdate();
+            JOptionPane.showMessageDialog(null, "Produto cadastrado com sucesso!", "Sucesso", JOptionPane.INFORMATION_MESSAGE);
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Não foi possível cadastrar o produto!\n" + e.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE);
+        }
+        
+    }
+    
+    public ArrayList<ProdutosDTO> listarProdutos(){
+        
+        try {
+            conn = new conectaDAO().connectDB();
+            prep = conn.prepareStatement("SELECT * FROM `produtos`");
+            resultset = prep.executeQuery();
+            while (resultset.next()) {
+                ProdutosDTO produto = new ProdutosDTO();
+                produto.setId(resultset.getInt("id"));
+                produto.setNome(resultset.getString("nome"));
+                produto.setValor(resultset.getInt("valor"));
+                produto.setStatus(resultset.getString("status"));
+                listagem.add(produto);
+            }
+            return listagem;
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Erro ao buscar produtos: " + e.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE);
+            return null;
+        }
+        
+    }
+    
+    
+    
+        
+}
+
---
+++ LeiloesTDSat/src/conectaDAO.java
@@ -0,0 +1,34 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Adm
+ */
+public class conectaDAO {
+    
+    public Connection connectDB(){
+        Connection conn = null;
+        
+        try {
+        
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/uc11?user=root&password=Casa2509//&useSSL=false");
+            
+        } catch (SQLException erro){
+            JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
+        }
+        return conn;
+    }
+    
+}
